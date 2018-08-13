e265a37bd2b1d67f277a53eaed81436d1e56116f
Kayvan Vakili <kvakili813@gmail.com>
2018-08-13 03:24:52 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < Sinatra::Base
   set :views, Proc.new { File.join(root, "../views/") }
   register Sinatra::Twitter::Bootstrap::Assets
 
-  get '/' do
-    erb :'application/index'
-  end
+  # #get '/' do
+  #   erb :'application/index'
+  # end
 end
