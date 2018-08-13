37c7e6d440bf55303ceeff118ce5e0bf5335b5e9
Kayvan Vakili <kvakili813@gmail.com>
2018-08-13 01:56:52 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,45 +1,39 @@
 class FiguresController < ApplicationController
+
   get '/figures' do
     @figures = Figure.all
     erb :'figures/index'
   end
 
-  get '/figures/new' do
+  #new
+  get '/figures/new' do 
     erb :'figures/new'
+  end  
+
+  post '/figures' do 
+
+    @figure = Figure.create(name: params[:name]) 
+    redirect to '/figures/#{@figure.id}'
   end
 
+  #show
   get '/figures/:id' do
     @figure = Figure.find_by_id(params[:id])
     erb :'figures/show'
   end
 
-  post '/figures' do
-    figure = Figure.create(params[:figure])
-    unless params[:title][:name].empty?
-      figure.titles.create(params[:title])
-    end
-    unless params[:landmark][:name].empty?
-      figure.landmarks.create(params[:landmark])
-    end
-
-    redirect "/figures/#{figure.id}"
-  end
-
+  #edit
   get '/figures/:id/edit' do
     @figure = Figure.find_by_id(params[:id])
     erb :'figures/edit'
   end
 
-  patch '/figures/:id' do
-    figure = Figure.find_by_id(params[:id])
-    figure.update(params[:figure])
-    unless params[:title][:name].empty?
-      figure.titles.create(params[:title])
-    end
-    unless params[:landmark][:name].empty?
-      figure.landmarks.create(params[:landmark])
-    end
-    redirect "/figures/#{figure.id}"
+  post '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id]) #should I find it by name?
+   @figure = Figure.update(name: params[:name])
+    redirect to '/figures/#{@figure.id}'
+  
   end
+
 
 end
